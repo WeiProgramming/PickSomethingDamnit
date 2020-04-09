cd4adbeca44c0f86333b60e70c109bb2d8337d32
WeiProgramming <wei.leung1995@gmail.com>
2020-04-09 14:57:14 -0700

    moved firebase to service folder

--- src/server/config.js
+++ src/server/config.js
@@ -1,6 +1,9 @@
 const config = {
     yelp: {
         apikey: 'bearer hdsg8CfSS3niYRqQq2O_EAiKfSOuBYyPTjEyoD5QIl24hUL88PcF2dx2oBCa_RZy5b581T3Hpf_g3XxH4Rw4T0L6CJXnKoDdNMb0Q5wjNnx4hMbFjkY9Ukh-FXhoXnYx'
+    },
+    googleMaps: {
+        apikey: 'AIzaSyBkuGkLW-kAdNjXh2ZJQlJM5OKfmc_6Xkk'
     }
 }
 
